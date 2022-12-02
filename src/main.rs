cf06aa0b2b64686ae179b058e17a1b1eeb27223f
Matt Godbolt <matt.godbolt@aquatic.com>
2022-12-02 13:53:43 -0600

    More elegant

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::process::Command;
 use std::process::exit;
 
 use clap::{crate_authors, crate_description, crate_version, Parser};
-use eyre::{ContextCompat, eyre, Report, Result};
+use eyre::{ContextCompat, eyre, Report, Result, WrapErr};
 use serde::Deserialize;
 
 #[derive(Parser, Debug)]
@@ -56,47 +56,50 @@ struct PyConfig {
     pre_run: Option<String>,
 }
 
-fn run() -> Result<()> {
-    let args = Opts::parse();
-    let path = Path::new(&args.script);
-    if !path.is_file() {
-        return Err(eyre!("Unable to open input file: {}", path.display()));
+fn pre_run(run_dir: &Path, pre_run_cmd: &str, verbose: bool) -> Result<()> {
+    if verbose {
+        println!("running pre_run: {}", pre_run_cmd);
     }
-    let toml = find_toml(path.parent().wrap_err("Unable to get parent dir")?)
-        .expect(format!("Unable to find a pyproject.toml for {}", path.display()).as_str());
-    let project_root = toml.parent().unwrap();
-    let toml_doc = fs::read_to_string(toml.as_path())?;
+    let args = ["-eu", "-o", "pipefail", "-c", &pre_run_cmd];
+    let res = Command::new("bash")
+        .args(args)
+        .current_dir(run_dir)
+        .status()?;
+    if !res.success() {
+        return Err(eyre!("Pre-run step '{}' failed with exit code {}", pre_run_cmd, res.code().unwrap()));
+    }
+    Ok(())
+}
+
+fn run() -> Result<()> {
+    let cmdline_args = Opts::parse();
+    let script_path = Path::new(&cmdline_args.script);
+    if !script_path.is_file() {
+        return Err(eyre!("Unable to open input file: {}", script_path.display()));
+    }
+    let toml = find_toml(script_path.parent().wrap_err("Unable to get script parent dir")?)
+        .wrap_err_with(|| format!("Unable to find a pyproject.toml for {}", script_path.display()))?;
+    let project_root = toml.parent().wrap_err("Unable to get project root")?;
+    let toml_doc = fs::read_to_string(toml.as_path()).wrap_err("Unable to read pyproject.toml")?;
     let config: Config = toml::from_str(&toml_doc)
-        .expect("Unable to read toml document or find the rpy.tool configuration in it");
+        .wrap_err("Unable to read toml document or find the rpy.tool configuration in it")?;
     let py_config = config.tool.rpy;
     let python = project_root.join(Path::new(&py_config.interpreter));
     let src_root = project_root.join(Path::new(&py_config.source_root));
-    if args.verbose {
+    if cmdline_args.verbose {
         println!("python: {}", python.display());
         println!("src_root: {}", src_root.display());
     }
     match py_config.pre_run {
-        Some(str) => {
-            if args.verbose {
-                println!("running pre_run: {}", str);
-            }
-            let args = ["-eu", "-o", "pipefail", "-c", &str];
-            let res = Command::new("bash")
-                .args(args)
-                .current_dir(project_root)
-                .status()?;
-            if !res.success() {
-                return Err(eyre!("Pre-run step '{}' failed with exit code {}", str, res.code().unwrap()));
-            }
-        }
+        Some(str) => { pre_run(project_root, &str, cmdline_args.verbose)?; }
         None => {}
     }
 
     // would be nice if I could work out how to prepend in-place which I know must be possible, like
     // a ranges thing I can pass to args. I nearly got it working with slices but I suck.
     // .args(&[&path[..], &args.args[..]].concat()) something like that?
-    let mut zomg = args.args.clone();
-    zomg.insert(0, String::from(path.to_str().unwrap()));
+    let mut zomg = cmdline_args.args.clone();
+    zomg.insert(0, String::from(script_path.to_str().unwrap()));
     Err(Report::new(Command::new(python)
         .args(&zomg)
         .env("PYTHONPATH", &src_root)
